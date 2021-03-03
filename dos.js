1d9c2230464090503594f89d3d937dca1ab98576
Agustin-Defranchi <agustin.i.defranchi@gmail.com>
2021-03-03 10:29:12 -0300

    finalizado

--- dos.js
+++ dos.js
@@ -6,156 +6,137 @@ function mostrar()
 	let sexo;
 	let nota;
 	let edad;
+	let flag;
+	let promMax;
+	let cantNotas;
+	let contNotas;
+	let prom;
+	let nombreMax;
+	let flagEdad;
+	let edadMin;
+	let nombreMin;
+	let promF;
+	let promM;
+	let acumNotaF;
+	let acumnotaM;
+	let cantF;
+	let cantM;
+	let flagmateris;
+		let materiaMax;
+		let edadMatMax;
+		let nombreMatMax;
+	flagmateris =0;
+
+
+	cantF=0;
+	cantM=0;
+
+		acumnotaM=0;
+		acumNotaF=0;
+	promM=0;
+	promF =0;
+	flagEdad=0
+	flag = 0;
+	contNotas=0;
+	prom = 0;
 
 	do
 	{
 		nombre = prompt("Ingrese su nombre:");
- 		cursada = prompt("Ingrese tipo de cursada('libre', 'presencial', 'remota'):");
-
-		cantmaterias = prompt("Ingrese cantidad de materias(mas de 0, menos de 8):");
-
- 		sexo = prompt("Ingrese sexo (femenino(f)/masculino(m)/no binario(n)):").toLowerCase();
- 		while(sexo != "masculino" && sexo != "femenino")
- 		{
- 		  sexo = prompt("Dato inválido.Ingrese sexo (femenino(f)/masculino(m)/no binario(n)):");
- 		}
-
-			notaprom = parseInt(prompt("Ingrese la nota promedio(entre 0 y 10):"));
-		while(!(notaprom>=0 && notaprom<=10))
+ 		
+ 		cursada = prompt("estado de su cursada(libre/presencial/remota) ");
+	  	while(cursada != "libre" && cursada != "presencial" && cursada != "remota")
 		{
- 		  notaprom = parseInt(prompt("Dato inválido. Ingrese la nota promedio(entre 0 y 10):"));
- 		}
+ 			cursada = prompt("ingrese un estado valido(libre/presencial/remota) ");
 
-		 edad = parseInt(prompt("Ingrese la edad:"));
- 		 while( edad>= 0 && isNaN(edad)){
-   		 edad = parseInt(prompt("Dato inválido. Ingrese edad."))
-	  }
+		}
+		cantidad = parseInt(prompt("ingrese la cantidad de materias(1 - 7)"));
+		while(cantidad <= 1 || cantidad >= 8 )
+		{
+			cantidad = parseInt(prompt("ingrese una cantidad dentro de los limites(1 -  7"));
+
+		}
+
+		sexo = prompt("ingrese su sexo (f/m)");
+		while(sexo != "f" && sexo != "m")
+		{
+			sexo = prompt("ingrese un sexo valido (f/m)");
+
+		}
+
+		nota= parseInt(prompt("ingrese una nota del 0-10"))
+		while(nota <0 || nota >10 )
+		{
+			nota= parseInt(prompt("ingrese una nota valida 0-10"))
+
+		}
+
+		edad = prompt("ingrese una edad (18 o mas)");
+		while(edad < 18)
+		{
+			edad = prompt("ingrese una edad valida (18 o mas)");
+		}
+		if(sexo == "s")
+		{
+			acumNotaF += nota;
+			cantF ++ ;
+			if(flag==0 || nota>promMax)
+			{
+				flag == 1;
+				promMax = nota;
+				nombreMax = nombre;
+					
+			}
+		}
+		else
+		{
+			acumnotaM +=nota;
+			cantM++;
+		}
+		if(cursada == "libre")
+		{
+			if(flagEdad ==0 || edad < edadMin)
+			{
+				flagEdad==1;
+				nombreMin = nombre;
+			}
+		}
+		if(flagmateris==0|| cantidad>materiaMax)
+		{
+			if(cursada != "remota")
+			{
+				flag == 1;
+				edadMatMax=edad;
+				nombreMatMax=nombre;
+
+			}
+		}
+
+
 
 		salir = prompt("desea salir?");
 	}while(salir != "s")
+
+	alert("el nombre del mejor promedio " + nombreMax);
+	alert("el nombre del alumno mas joven querinde libre es "+nombreMin);
+	promF=acumNotaF/cantF;
+	promM=acumnotaM/cantM;
+	alert("el promedio masculino es "+promM + " y  el orimedio femenino es "+promF);
+	alert("el nombre y la edad del que cursa mas materias de forma no remota es" +nombreMatMax +" y "+edadMatMax+"años");
+
+
+
 }
 /*
 Enunciado:
 
-Realizar el algoritmo que permita ingresar los datos de los alumnos de una division de la UTN FRA, los datos solicitados son:
-nombre
-Tipo curasada("libre";"presencial";"remota")
-cantidad de materias( mas de cero y menos de 8)
-Sexo ( femenino , masculino , no binario)
-Nota promedio (entre 0 y 10)
-edad (validar)
+
 se debe informar de existir, o informar que no existe , en el caso que corresponda.
 a) El nombre del mejor promedio que no sea masculino
 b) El nombre del mas joven de los alumnos entre los que la dan libre
+
 c) El promedio de nota por sexo
 d) La edad y nombre del que cursa mas materias que no sean en forma remota
 
-let cursada;
-let cantmaterias;
-let sexo;
-let notaprom;
-let edad;
-let seguir = "s";
-let prommax;
-let nomprom;
-let nombre;
-let nomlibre;
-let edadmin;
-let acumpromM = 0;
-let acumpromF = 0;
-let acumpromN = 0;
-let promM;
-let promF;
-let promN;
-let alumM;
-let alumF;
-let alumN;
-let totalmaterias;
-let materiasMax;
-let nombremat;
-let edadmat;
 
-do{
-  nombre = prompt("Ingrese su nombre:");
-
-  cursada = prompt("Ingrese tipo de cursada('libre', 'presencial', 'remota'):");
-
-  cantmaterias = prompt("Ingrese cantidad de materias(mas de 0, menos de 8):");
-
-  sexo = prompt("Ingrese sexo (femenino(f)/masculino(m)/no binario(n)):").toLowerCase();
-  while(sexo != "m" && sexo != "f" && sexo != "n"){
-    sexo = prompt("Dato inválido.Ingrese sexo (femenino(f)/masculino(m)/no binario(n)):");
-  }
-
-  notaprom = parseInt(prompt("Ingrese la nota promedio(entre 0 y 10):"));
-  while(!(notaprom>=0 && notaprom<=10)){
-    notaprom = parseInt(prompt("Dato inválido. Ingrese la nota promedio(entre 0 y 10):"));
-  }
-
-  edad = parseInt(prompt("Ingrese la edad:"));
-  while( edad>= 0 && isNaN(edad)){
-    edad = parseInt(prompt("Dato inválido. Ingrese edad."))
-  }
-
-  if(sexo != "m"){
-    if(prom>prommax){
-      prommax = prom;
-      nomprom = nombre;
-    }
-
-  }
-
-  if(cursada == "libre"){
-    if(edad<edadmin){
-      edadmin = edad;
-      nomlibre = nombre;
-    }
-
-  }
-
-  if(sexo == "m"){
-    acumpromM += notaprom;
-    alumM++;
-  }else if(sexo == "f"){
-    acumpromF += notaprom;
-    alumF++;
-  }else{
-    acumpromN += notaprom
-    alumN++;
-  }
-
-  promM = acumpromM / alumM;
-  promF = acumpromF / alumF;
-  promN = acumpromN / alumN;
-
-  if(cursada != "remota"){
-    totalmaterias + cantmaterias;
-    if(totalmaterias>materiasMax){
-      materiasMax = totalmaterias;
-      nombremat = nombre;
-      edadmat = edad;
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-
-  seguir = prompt("Desea seguir ingresando productos?  s/n");
-
-}while( seguir == "s")
-
-console.log("El nombre del mejor promedio no masculino es:" + nomprom +"<br>")
-console.log("El nombre del más joven de los alumnos que la dan libre es:" + nomlibre +"<br>")
-console.log("El promedio de nota por sexo es: masculino:" + promM + "femenino:" + promF + "no binario:" + promN +"<br>" )
-console.log("La edad y el nombre del que curse más materias de forma no remota es:" + nombremat + "y tiene" + edadmat + "años." + "<br>")
-
-}
 */
